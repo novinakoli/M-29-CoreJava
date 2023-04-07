1eff4a4ab620b1451797fc62b1a08c3d0fc2cb81
OM SAI <OM SAI@DESKTOP-VC4G1GR>
2023-04-07 21:00:40 +0530

    second commit

---
+++ daytwo/src/org/stjohn/looping/EnhancedLoop.java
@@ -0,0 +1,14 @@
+//program to demonstrate on for enhanced loop
+package org.stjohn.looping;
+
+public class EnhancedLoop {
+
+	public static void main(String[] args) {
+		char array[] = {'a','b','c'};
+		for(int itr:array) {
+			System.out.print(itr+" ");
+		}
+
+	}
+
+}
