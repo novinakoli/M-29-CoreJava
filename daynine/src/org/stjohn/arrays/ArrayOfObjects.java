4822caa52ae6727cfa1b335e4b666fcd806a8e03
OM SAI <OM SAI@DESKTOP-VC4G1GR>
2023-05-03 17:06:12 +0530

    first commit

---
+++ daynine/src/org/stjohn/arrays/ArrayOfObjects.java
@@ -0,0 +1,18 @@
+package org.stjohn.arrays;
+
+public class ArrayOfObjects {
+
+	public static void main(String[] args) {
+		Customer[] arr = new Customer[2]; //array of objects
+		
+		arr[0] = new Customer(123, "Novina");
+		arr[1] = new Customer(456, "Vaishnavi");
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(arr[i].getId()+" "+arr[i].getName());
+		}
+		
+
+	}
+
+}
